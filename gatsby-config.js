85fe984be31477947ee8e54f1a72b59ec48a560f
Ross Neilson <rossneilson99@gmail.com>
2020-06-07 17:22:04 +0100

    adding service worker back again

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
         theme_color: `#2096f3c7`,
         display: `standalone`,
         icon: `static/icon-512x512.png`,
+        cache_busting_mode: "none",
       },
     },
     {
@@ -56,11 +57,18 @@ module.exports = {
       },
     },
     {
+      resolve: "gatsby-plugin-offline",
+      options: {
+        workboxConfig: {
+          globPatterns: ["**/*"],
+        },
+      },
+    },
+    {
       resolve: `gatsby-plugin-styled-components`,
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-remove-serviceworker`,
   ],
 }
