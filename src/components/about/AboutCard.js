61a64e5c6b3ace28ab08e562e765c81af0a71219
Ross Neilson <39137239+rossneilson@users.noreply.github.com>
2023-01-21 16:19:10 +0000

    Update AboutCard.js

--- src/components/about/AboutCard.js
+++ src/components/about/AboutCard.js
@@ -71,9 +71,6 @@ export default function AboutCard(props) {
         <p>
           <FormattedMessage id="about.line3" />
         </p>
-        <p>
-          <FormattedMessage id="about.line4" />
-        </p>
         <OutOfJapanWarning>
           <svg
             xmlns="http://www.w3.org/2000/svg"
